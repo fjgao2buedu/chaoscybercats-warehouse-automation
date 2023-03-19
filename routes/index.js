db917ca96c6ea591ded3c253b246c0313ae1a710
fjgao <fjgao@bu.edu>
2023-03-19 01:46:40 -0400

    using correct build

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.use(express.static(path.join('.','client','build2')));
+router.use(express.static(path.join('.','client','build')));
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
