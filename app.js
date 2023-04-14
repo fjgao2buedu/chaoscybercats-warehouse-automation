33836bcc1a63d541c18d6456f39d14c950d07242
fjgao <fjgao@bu.edu>
2023-04-13 21:07:42 -0400

    commenting out code

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+var indexRouter = require('./routes/index.js');
 var shippingdataRouter = require('./routes/shippingdata.js');
 var parsejsonRouter = require('./routes/parsejson.js')
 var dataentryRouter = require('./routes/dataentry.js')
 
 // swagger
-var swaggerUI = require('swagger-ui-express');
-const fs = require("fs")
-const YAML = require('yaml')
-const file  = fs.readFileSync('./swagger.yaml', 'utf8')
-const swaggerDocument = YAML.parse(file)
+// var swaggerUI = require('swagger-ui-express');
+// const fs = require("fs")
+// const YAML = require('yaml')
+// const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+// const swaggerDocument = YAML.parse(file)
 
 var app = express();
 const port = process.env.PORT || 8080;
@@ -22,14 +21,14 @@ const port = process.env.PORT || 8080;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // swagger
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', indexRouter);
 app.use('/shipping_data', shippingdataRouter);
@@ -56,4 +55,4 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port} .`);
   });
 
-module.exports = app;
+// module.exports = app;
