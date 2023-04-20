f624e400172215cdcd2710942e6ac2776f274f5e
fjgao <fjgao@bu.edu>
2023-04-20 19:31:43 -0400

    code clean up

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-var express = require('express');
-var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-var indexRouter = require('./routes/index.js');
-var shippingdataRouter = require('./routes/shippingdata.js');
-var parsejsonRouter = require('./routes/parsejson.js')
-var dataentryRouter = require('./routes/dataentry.js')
-
-// swagger
-// var swaggerUI = require('swagger-ui-express');
-// const fs = require("fs")
-// const YAML = require('yaml')
-// const file  = fs.readFileSync('./swagger.yaml', 'utf8')
-// const swaggerDocument = YAML.parse(file)
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index.js');
+const analyzeresultRouter = require('./routes/analyzeresult.js')
+const dataentryRouter = require('./routes/dataentry.js')
+const parsejsonRouter = require('./routes/parsejson.js')
+const shippingdataRouter = require('./routes/shippingdata.js');
 
 var app = express();
 const port = process.env.PORT || 8080;
@@ -23,36 +15,15 @@ app.set('view engine', 'hbs');
 
 // app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// swagger
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', indexRouter);
-app.use('/shipping_data', shippingdataRouter);
-app.use('/parsejson',parsejsonRouter);
-app.use('/dataentry',dataentryRouter);
+app.use('/analyzeresult', analyzeresultRouter);
+app.use('/dataentry', dataentryRouter);
+app.use('/parsejson', parsejsonRouter);
+app.use('/shippingdata', shippingdataRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port} .`);
-  });
+  console.log(`Example app listening at http://localhost:${port} .`);
+});
 
-// module.exports = app;
